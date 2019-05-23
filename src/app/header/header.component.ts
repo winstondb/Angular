6bc917de941e4e03bffc45fae0ef720d257f11a2
Winston Dantas Barbosa <winstondb@hotmail.com>
2019-05-23 11:45:38 -0300

    Initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router'
+
+@Component({
+  selector: 'mt-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
